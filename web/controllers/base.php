6d0bf76fb80817215058c9ead1394a3f3dd17ae1
ZUNbado <git@acs.li>
2014-05-26 21:28:14 +0200

    changes

--- web/controllers/base.php
+++ web/controllers/base.php
@@ -10,9 +10,31 @@
  * file that was distributed with this source code.
  */
 
+
 require_once __DIR__.'/../../vendor/autoload.php';
 require_once __DIR__.'/../../src/app.php';
 
+
+require_once __DIR__.'/apache2_namevirtualhost/index.php';
+require_once __DIR__.'/apache2_vhosts/index.php';
+require_once __DIR__.'/group/index.php';
+require_once __DIR__.'/group_vars/index.php';
+require_once __DIR__.'/host/index.php';
+require_once __DIR__.'/host_group/index.php';
+require_once __DIR__.'/host_vars/index.php';
+require_once __DIR__.'/interfaces/index.php';
+require_once __DIR__.'/interfaces_rules/index.php';
+require_once __DIR__.'/iptables_policies/index.php';
+require_once __DIR__.'/iptables_rules/index.php';
+require_once __DIR__.'/roletable/index.php';
+require_once __DIR__.'/single_var/index.php';
+require_once __DIR__.'/sshkeys/index.php';
+require_once __DIR__.'/sudoers/index.php';
+require_once __DIR__.'/zabbix_userparameters/index.php';
+
+if(file_exists(__DIR__.'/custom.php')){
+        require_once __DIR__.'/custom.php';
+}
 
 $app->match('/', function () use ($app) {
 
